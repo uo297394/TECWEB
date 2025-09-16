d3df294fa684a3c43248c48f43f5c1367fc52aaa
uo297394 <uo297394@uniovi.es>
2025-09-16 18:49:47 +0200

    Mi primer commit

---
+++ src/main/java/es/tew/HolaMundoServlet.java
@@ -0,0 +1,26 @@
+package es.tew;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/HolaMundo")
+
+public class HolaMundoServlet extends HttpServlet{
+    public void doGet (HttpServletRequest req, HttpServletResponse resp) throws IOException, 
+        ServletException {
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html"); 
+        PrintWriter out = resp.getWriter(); 
+        out.println("<HTML>"); 
+        out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>"); 
+        out.println("<BODY>"); 
+        out.println("Bienvenido a mi primera página web!"); 
+        out.println("</BODY></HTML>"); 
+    }
+}
